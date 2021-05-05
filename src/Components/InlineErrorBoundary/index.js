b8d3f18c92cd82a4340a1ce040af11efdd658866
Anand <seanjin17@gmail.com>
2021-05-05 19:13:30 +0530

    Create inline error boundary component

---
+++ src/Components/InlineErrorBoundary/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+export default function InlineError(props) {
+  return (
+    <div className="fadeInUp">
+      <div style={{ display: "flex", justifyContent: "center", marginTop: 20 }}>
+        <img src="/logo_main.png" alt="mainlogo" />
+      </div>{" "}
+      <div style={{ textAlign: "center" }}>
+        <h2>Oops! Something went wrong</h2>
+        <p style={{ color: "#cc0c0c", fontSize: "medium" }}>{props.error}</p>
+      </div>
+    </div>
+  );
+}

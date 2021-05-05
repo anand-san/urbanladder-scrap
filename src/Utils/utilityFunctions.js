df7b47edf84c714d623dc9ab3e505e8a36772090
Anand <seanjin17@gmail.com>
2021-05-05 20:50:32 +0530

    add json sort function

--- src/Utils/utilityFunctions.js
+++ src/Utils/utilityFunctions.js
@@ -1,10 +1,18 @@
-export const numberWithCommas = number => {
-    try{
-      if(number){
-        number = typeof(number) !== "string" ? number.toString() : number
-        return number.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-      }
-    }catch(e){
-      console.log(e)
+export const numberWithCommas = (number) => {
+  try {
+    if (number) {
+      number = typeof number !== "string" ? number.toString() : number;
+      return number.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
+  } catch (e) {
+    console.log(e);
   }
+};
+
+export function sort_by_key(array, key) {
+  return array.sort(function (a, b) {
+    var x = a[key];
+    var y = b[key];
+    return x < y ? -1 : x > y ? 1 : 0;
+  });
+}
